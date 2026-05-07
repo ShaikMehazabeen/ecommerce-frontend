280da8332147137a03612673787ee9409563b874
ShaikMehazabeen <shaikmehazabeen777@gmail.com>
2026-05-07 16:16:07 +0530

    Add header and navigation bar

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
+// Select Elements
+const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navbar');
+
+// Toggle Mobile Menu
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+const navLinks = document.querySelectorAll('.navbar ul li a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    });
+});
+
 console.log("E-Commerce Website Loaded");
